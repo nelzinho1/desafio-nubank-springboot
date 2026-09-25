0753e8db5aa4bb4090ec04d4f2535eda79e991d8
nelzinho1 <emanuelsouza1991@hotmail.com>
2026-09-25 17:13:40 -0300

    feat: Criando ClientController para salvar clientes.

--- src/main/java/desafio/nubank/springboot/controller/ClientController.java
+++ src/main/java/desafio/nubank/springboot/controller/ClientController.java
@@ -1,13 +1,15 @@
 package desafio.nubank.springboot.controller;
 
+import desafio.nubank.springboot.dto.ClientsRequestDto;
 import desafio.nubank.springboot.model.Clients;
 import desafio.nubank.springboot.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
@@ -16,8 +18,9 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
-    @GetMapping
-    public List<Clients> listAll() {
-        return clientService.listAll();
+    @PostMapping
+    public ResponseEntity<Clients> create(@RequestBody ClientsRequestDto dto) {
+        Clients createdClient = clientService.salveClients(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdClient);
     }
 }
--- src/main/java/desafio/nubank/springboot/service/ClientService.java
+++ src/main/java/desafio/nubank/springboot/service/ClientService.java
@@ -1,6 +1,8 @@
 package desafio.nubank.springboot.service;
 
 import desafio.nubank.springboot.dto.ClientsRequestDto;
+import desafio.nubank.springboot.dto.ClientsResponseDto;
+import desafio.nubank.springboot.dto.ContactsResponseDto;
 import desafio.nubank.springboot.model.Clients;
 import desafio.nubank.springboot.model.Contacts;
 import desafio.nubank.springboot.repository.ClientsRepository;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -34,8 +37,38 @@ public class ClientService {
         return clientsRepository.save(clients);
     }
 
-    public List<Clients> listAll() {
-        return clientsRepository.findAll().stream().map()
+    // listar todos os clientes com seus contatos.
+    public List<ClientsResponseDto> listAll() {
+        return clientsRepository.findAll().stream().map(this::toDto).collect(Collectors.toList());
     }
 
+    public List<ContactsResponseDto> contactListForClient(Long clientId) {
+
+        Clients clients = clientsRepository.findById(clientId)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado."));
+        return clients.getContacts().stream().map(c -> {
+            ContactsResponseDto contactDto = new ContactsResponseDto();
+            contactDto.setId(c.getId());
+            contactDto.setCel(c.getCel());
+            contactDto.setEmail(c.getEmail());
+            return contactDto;
+        }).collect(Collectors.toList());
+    }
+
+    private ClientsResponseDto toDto(Clients clients) {
+        ClientsResponseDto dto = new ClientsResponseDto();
+        dto.setId(clients.getId());
+        dto.setName(clients.getName());
+
+        List<ContactsResponseDto> contacts = dto.getContactsDto().stream().map(c -> {
+            ContactsResponseDto contactDto = new ContactsResponseDto();
+            contactDto.setId(c.getId());
+            contactDto.setCel(c.getCel());
+            contactDto.setEmail(c.getEmail());
+            return contactDto;
+        }).toList();
+        dto.setContactsDto(contacts);
+
+        return dto;
+    }
 }
